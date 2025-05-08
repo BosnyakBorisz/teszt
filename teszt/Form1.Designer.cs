5d92da31005e25c73fd4c236fb095d55e91154e0
BosnyakBorisz <bosnyakborisz@turr.hu>
2025-05-08 09:34:39 +0200

    alapok

--- teszt/Form1.Designer.cs
+++ teszt/Form1.Designer.cs
@@ -34,20 +34,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(178, 110);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(221, 153);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(153, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Regisztráció";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(178, 164);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(409, 153);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(153, 51);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Bejentkezés";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
